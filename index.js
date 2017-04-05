869eff18b539e2809ff9e4df5ffeab1208fc559f
eddicke <adenekanademola931@gmail.com>
2017-04-05 18:58:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,11 +8,63 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
+    console.log('i think i saw something...');
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
+ console.log('i think i saw something...');
+  });
+  //game try!
+  socket.on('ademola', function(gme){
+    io.emit('ademola', gme);
+  });
+  //power up
+   socket.on('power', function(hunts){
+    io.emit('power', hunts);
+  });
+  //power up2
+   socket.on('power2', function(power){
+    io.emit('power2', power);
+  });
+  //webcam
+socket.on('webcam', function(webcam){
+    io.emit('webcam', webcam);
+  });
+  //move down
+  socket.on('player2-1', function(down){
+    io.emit('player2-1', down);
+  });
+  //player2
+  socket.on('player2', function(hunts){
+    io.emit('player2', hunts);
+  });
+  //move down
+  socket.on('player2-1', function(down){
+    io.emit('player2-1', down);
+  });
+  //player1
+   socket.on('hunts', function(hunts){
+    io.emit('hunts', hunts);
+  });
+   socket.on('player1', function(hunts){
+    io.emit('player1', hunts);
+  });
+  //state-1
+  socket.on('state', function(stats){
+    io.emit('state', stats);
+  });
+   //state-2
+  socket.on('state2', function(stats){
+    io.emit('state2', stats);
+  });
+  //login
+  socket.on('login', function(login){
+    io.emit('login', login);
+  });
+  //move down
+  socket.on('player1-1', function(down){
+    io.emit('player1-1', down);
   });
 });
-
 http.listen(port, function(){
   console.log('listening on *:' + port);
 });
